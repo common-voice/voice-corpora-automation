c7831ce09b0cc50458805ad149bf63b8996abcb3
John Giannelos <johngiannelos@gmail.com>
2020-04-08 15:10:01 +0200

    Remove stale directory

--- voice-corpora-automation/__init__.py
+++
@@ -1,5 +0,0 @@
-"""voice-corpora-automation - Automation to improve the dataset export process for Common Voice"""
-
-__version__ = "0.1.0"
-__author__ = "John Giannelos <jgiannelos@mozilla.com>"
-__all__ = []
